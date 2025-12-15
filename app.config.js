f65f5c3ee1ab5ee21c4daef244729cb60ba570de
jjbmsda <jjbmsda@naver.com>
2025-12-15 13:48:44 +0900

    app.config.js app.json

--- app.config.js
+++ app.config.js
@@ -1,11 +1,17 @@
-import "dotenv/config";
-
 export default {
   expo: {
     name: "OTT Mood",
     slug: "ott-mood-app",
+
+    android: {
+      package: "com.oneul.moviemood",
+    },
+
     extra: {
       tmdbApiKey: process.env.TMDB_API_KEY,
+      eas: {
+        projectId: "98078928-0b4f-4d9e-8a4c-18731b8ea06b",
+      },
     },
   },
 };
